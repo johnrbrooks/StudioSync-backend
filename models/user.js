d9b51b99114bd1eb2e4403859a89f85b7eae230d
John Brooks <johnrbrooks93@gmail.com>
2023-07-26 12:09:21 -0400

    Updated user model

--- models/user.js
+++ models/user.js
@@ -2,9 +2,9 @@ const { Schema } = require('mongoose')
 
 const userSchema = new Schema (
     {
+        name: { type: String, required: true },
         username: { type: String, required: true },
         password: { type: String, required: true },
-        first_name: { type: String, required: true },
         pipeline: [{ type: Schema.Types.ObjectId, ref: "Prospect" }],
         mode: { type: Boolean }
     },
