0e4acd1014ad17780250d936992190cd316739cf
John Brooks <johnrbrooks93@gmail.com>
2023-07-26 11:33:10 -0400

    Fully removed pipeline model from backend

--- models/index.js
+++ models/index.js
@@ -1,20 +1,17 @@
 const mongoose = require("mongoose");
 
 const userSchema = require("./user");
-const pipelineSchema = require("./pipeline");
 const prospectSchema = require("./prospect.js");
 const calendarSchema = require("./calendar");
 const todoSchema = require ("./todo");
 
 const User = mongoose.model("User", userSchema);
-const Pipeline = mongoose.model("Pipeline", pipelineSchema);
 const Prospect = mongoose.model("Prospect", prospectSchema);
 const Calendar = mongoose.model("Calendar", calendarSchema);
 const Todo = mongoose.model("Todo", todoSchema)
 
 module.exports = {
   User,
-  Pipeline,
   Prospect,
   Calendar,
   Todo
