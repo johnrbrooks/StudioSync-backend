ad0e55a1a48cbec0a18969dd1a243f2e63ed569b
John Brooks <johnrbrooks93@gmail.com>
2023-08-07 11:40:22 -0400

    Final tidying

--- models/index.js
+++ models/index.js
@@ -2,17 +2,11 @@ const mongoose = require("mongoose");
 
 const userSchema = require("./user");
 const prospectSchema = require("./prospect.js");
-const calendarSchema = require("./calendar");
-const todoSchema = require ("./todo");
 
 const User = mongoose.model("User", userSchema);
 const Prospect = mongoose.model("Prospect", prospectSchema);
-const Calendar = mongoose.model("Calendar", calendarSchema);
-const Todo = mongoose.model("Todo", todoSchema)
 
 module.exports = {
   User,
   Prospect,
-  Calendar,
-  Todo
 };
